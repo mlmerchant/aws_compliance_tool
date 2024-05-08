6b45c51b4a6164d5d76b9bbbb315354297167c6b
Matt Merchant <matthew.lee.merchant@gmail.com>
2024-05-08 14:16:41 -0500

    Update iam_roles.py

--- src/modules/provider/iam_roles.py
+++ src/modules/provider/iam_roles.py
@@ -2,8 +2,8 @@ import boto3
 
 roles = dict()
 
-def get_iam_roles() -> None:
-    paginator = iam_client.get_paginator('list_roles')
-    for page in paginator.paginate():
-        for role in page['Roles']:
-            roles[role['RoleName']] = role
+get_iam_roles() -> None:
+paginator = iam_client.get_paginator('list_roles')
+for page in paginator.paginate():
+    for role in page['Roles']:
+        roles[role['RoleName']] = role
