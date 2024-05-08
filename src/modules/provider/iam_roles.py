d7ac1ef94072814cddfada90c8e503b6831714c2
Matt Merchant <matthew.lee.merchant@gmail.com>
2024-05-08 13:46:49 -0500

    Create iam_roles.py

---
+++ src/modules/provider/iam_roles.py
@@ -0,0 +1,9 @@
+import boto3
+
+roles = dict()
+
+def get_iam_roles() -> None:
+    paginator = iam_client.get_paginator('list_roles')
+    for page in paginator.paginate():
+        for role in page['Roles']:
+            roles[role['RoleName']] = role
