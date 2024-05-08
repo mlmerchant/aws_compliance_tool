a9deb4504c99f1732447c696a0d1dc3d83107966
Matt Merchant <matthew.lee.merchant@gmail.com>
2024-05-08 12:20:18 -0500

    Create finding_id.py
    initial commit for figuring out how to create unique finding ids.

---
+++ src/modules/findings/finding_id.py
@@ -0,0 +1,25 @@
+import hashlib
+
+def generate_finding_arn(product_name: str, resource_arn: str, security_control_id: str) -> str:
+
+  def hash_key(string: str) -> str:
+    hash_object = hashlib.md5()
+    hash_object.update(string.encode())
+    hex_digits = hash_object.hexdigest()
+
+    # only keep half of the hash, to make it easier to use but still very unlikely for collisions.
+    hex_digits = hex_digits[0:16]
+
+    return hex_digits
+
+  hash_this = product_name + resource_arn + security_control_id
+  hashed = hash_key(hash_this)
+
+  resource_arn_split = resource_arn.split(:) 
+  partition = resource_arn_split[1]
+  account = resource_arn_split[4]
+  product_name = product_name.replace(' ','_')
+  
+  finding_arn = f"arn:{partition}:{product_name}:{region}:{account}/security-control/{security_control_id}/finding/{hashed}"
+
+  return finding_arn
