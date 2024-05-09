08bedf040822a7a6b0c1177b9c77b78ab2a40834
Matthew Merchant <matthew.lee.merchant@gmail.com>
2024-05-09 13:25:57 -0500

    Changing naming standard for resource dicts

--- src/modules/provider/iam_instance_profiles.py
+++ src/modules/provider/iam_instance_profiles.py
@@ -1,10 +1,10 @@
 import boto3
 
-instance_profiles = dict()
+iam_instance_profiles = dict()
 
 def get_instance_profiles() -> None:
   # creates map of instance profile arn to profile details
   paginator = iam_client.get_paginator('list_instance_profiles')
   for page in paginator.paginate():
     for profile in page['InstanceProfiles']:
-      instance_profiles[profile['Arn']] = profile
+      iam_instance_profiles[profile['Arn']] = profile
--- src/modules/provider/iam_role_to_policy_mapping.py
+++ src/modules/provider/iam_role_to_policy_mapping.py
@@ -2,9 +2,9 @@ import boto3
 import modules.provider.iam_roles as iam_roles
 
 # Policy name to list of attached roles
-role_to_polices = dict()
+iam_role_to_polices = dict()
 
 for role in iam_roles.roles:
-    role_to_policies[role] = iam_client.list_attached_policies(RoleName=role)['AttachedPolicies']
+    iam_role_to_policies[role] = iam_client.list_attached_policies(RoleName=role)['AttachedPolicies']
      
     
