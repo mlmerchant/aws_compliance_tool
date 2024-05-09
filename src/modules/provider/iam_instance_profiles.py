d0cb4007b095cbd01450b7166769e34f444372d4
Matthew Merchant <matthew.lee.merchant@gmail.com>
2024-05-09 13:27:54 -0500

    Changing way resources are imported

--- src/modules/provider/iam_instance_profiles.py
+++ src/modules/provider/iam_instance_profiles.py
@@ -2,9 +2,8 @@ import boto3
 
 iam_instance_profiles = dict()
 
-def get_instance_profiles() -> None:
-  # creates map of instance profile arn to profile details
-  paginator = iam_client.get_paginator('list_instance_profiles')
-  for page in paginator.paginate():
-    for profile in page['InstanceProfiles']:
-      iam_instance_profiles[profile['Arn']] = profile
+# creates map of instance profile arn to profile details
+paginator = iam_client.get_paginator('list_instance_profiles')
+for page in paginator.paginate():
+for profile in page['InstanceProfiles']:
+  iam_instance_profiles[profile['Arn']] = profile
